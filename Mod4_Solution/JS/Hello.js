3d88d8ef0ad8551712e6ab25174731aa150ed566
selvasachein <120453887+selvasachein@users.noreply.github.com>
2024-07-10 11:54:59 +0530

    Add files via upload

---
+++ Mod4_Solution/JS/Hello.js
@@ -0,0 +1,7 @@
+var helloSpeaker = {};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+  window.helloSpeaker = helloSpeaker;
+})(window);
