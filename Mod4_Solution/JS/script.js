396711111fa19b1db25a4b957e2f9310fc925d82
selvasachein <120453887+selvasachein@users.noreply.github.com>
2024-07-10 12:00:42 +0530

    Update script.js

--- Mod4_Solution/JS/script.js
+++ Mod4_Solution/JS/script.js
@@ -1,4 +1,4 @@
-function(){
+(function(){
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for ( var i = 0; i < names.length; i++) {
       var firstLetter =names[i].charAt(0);
@@ -8,4 +8,4 @@ function(){
         helloSpeaker.speak(names[i]);
       }
     }
-  }
+  })()
